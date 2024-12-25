d7baa6fc930bcf7b9f07d6386579b7a2dbd57ffb
grantsanders <grantsanders89@gmail.com>
2024-12-24 19:49:22 -0500

    fixed Game update method

--- jinx-debt-api/Models/Game.cs
+++ jinx-debt-api/Models/Game.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace jinx_debt_api.Models;
 
 public class Game
@@ -7,6 +9,8 @@ public class Game
     public int Player2_ID { get; set; }
     public int Player1_Score { get; set; }
     public int Player2_Score { get; set; }
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
     public int? Advantage { get; set; }
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)] 
     public DateTime LastModifyDate { get; set; }
 }
